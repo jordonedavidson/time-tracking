18cff42c7f14bd0cc657493aa711578e033b0893
Jordon Davidson <jodavidson@mta.ca>
2023-07-04 15:11:43 -0300

    Successfully implemented a listing of the time types on the frontend

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import { invoke } from "@tauri-apps/api/tauri";
 
+type TimeType  = {
+  id: Number,
+  label: String,
+  default_hours: Number,
+}
+
 let greetInputEl: HTMLInputElement | null;
 let greetMsgEl: HTMLElement | null;
+let listingEl: HTMLDivElement | null;
 
 async function greet() {
   if (greetMsgEl && greetInputEl) {
@@ -12,11 +19,31 @@ async function greet() {
   }
 }
 
+async function timeTypeListing() {
+  if (listingEl) {
+    const timeTypes = await invoke("list_timetypes")
+      .catch(e => {
+        const p = document.createElement('p')
+        p.innerText = e.message
+        p.className = 'error'
+        listingEl?.appendChild(p)
+      }) as Array<TimeType>
+
+    timeTypes.forEach(t => {
+      const p = document.createElement('p')
+      p.innerHTML = `<b>${t.id}</b>: ${t.label} - ${t.default_hours}`
+      listingEl?.appendChild(p)
+    })
+  }
+}
+
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
+  listingEl = document.querySelector("#timetypes");
   document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
     e.preventDefault();
     greet();
   });
+  timeTypeListing();
 });
